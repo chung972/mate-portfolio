721b6da24cd0a6f2b6b2a0a03ef0e5272baa89c9
Albert Chung <chung972@gmail.com>
2019-07-27 18:31:15 -0500

    changes hexademical values for website colors

--- colors.js
+++ colors.js
@@ -1,12 +1,12 @@
 module.exports = {
   background: '#FFFFFF',
-  backgroundDark: '#f0e6f6',
+  backgroundDark: '#f2f2f2',
 
-  primary: '#7c37ad',
-  primaryLight: '#ae66df',
-  primaryDark: '#4b007d',
+  primary: '#175296',
+  primaryLight: '#447ab8',
+  primaryDark: '#123c6e',
 
-  secondary: '#ff4081',
-  secondaryLight: '#ff79b0',
-  secondaryDark: '#c60055',
+  secondary: '#d11d1d',
+  secondaryLight: '#db4242',
+  secondaryDark: '#bd0000',
 };
